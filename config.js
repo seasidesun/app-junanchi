8d43ddb51cddc4c3400604ad3109e9e37cfd4aab
haibin <sunhaibin@goyoo.com>
2016-12-26 18:12:32 +0800

    init

---
+++ config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+    // 程序运行的端口
+    express: {
+        port: process.env.PORT || 3000,
+    },
+    mongo: {
+        url: 'mongodb://localhost/app-junanchi'
+    }
+}
